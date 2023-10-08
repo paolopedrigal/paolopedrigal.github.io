99405570e04c6836af917d0dca95598def7b8383
Paolo Pedrigal <paolopedrigal@gmail.com>
2023-10-08 15:24:46 -0700

    Added text appearing animation as well as mouse hovering over nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import About from "./About/About";
 import Projects from "./Projects/Projects";
 import Contact from "./Contact/Contact";
@@ -7,9 +7,17 @@ import resume from "./Resume_PaoloPedrigal.pdf";
 import "./App.css";
 
 function App() {
+  const paoloTitleRef: any = useRef(null);
+  const pedrigalTitleRef: any = useRef(null);
+  const navRef: any = useRef(null);
+  const aboutNavRef: any = useRef(null);
+  const projectsNavRef: any = useRef(null);
+  const resumeNavRef: any = useRef(null);
+  const contactNavRef: any = useRef(null);
   const aboutRef: any = useRef(null);
   const projectsRef: any = useRef(null);
   const contactRef: any = useRef(null);
+  const landingRefs: any[] = [paoloTitleRef, pedrigalTitleRef, navRef];
 
   const scrollTo = (ref: any) => {
     ref.current.scrollIntoView({
@@ -19,31 +27,80 @@ function App() {
     });
   };
 
+  const addUnderline = (ref: any) => {
+    ref.current.style.textDecoration = "underline";
+  };
+
+  const removeUnderline = (ref: any) => {
+    ref.current.style.textDecoration = "none";
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        }
+      });
+    });
+
+    landingRefs.forEach((ref) => {
+      observer.observe(ref.current);
+    });
+  }, landingRefs);
+
   return (
     <div>
       <div className="landing-image-container">
         <img src={FrontPage} alt="Landing Image" className="landing-image" />
         <div className="landing-title-container">
-          <h1 className="landing-title" id="paolo-title">
+          <h1 className="landing-title" id="paolo-title" ref={paoloTitleRef}>
             &nbsp;paolo
           </h1>
-          <h1 className="landing-title" id="pedrigal-title">
+          <h1
+            className="landing-title"
+            id="pedrigal-title"
+            ref={pedrigalTitleRef}
+          >
             pedrigal
           </h1>
         </div>
-        <ul className="nav">
-          <li className="nav-item" onClick={() => scrollTo(aboutRef)}>
+        <ul className="nav" ref={navRef}>
+          <li
+            className="nav-item"
+            ref={aboutNavRef}
+            onClick={() => scrollTo(aboutRef)}
+            onMouseEnter={() => addUnderline(aboutNavRef)}
+            onMouseLeave={() => removeUnderline(aboutNavRef)}
+          >
             about
           </li>
-          <li className="nav-item" onClick={() => scrollTo(projectsRef)}>
+          <li
+            className="nav-item"
+            ref={projectsNavRef}
+            onClick={() => scrollTo(projectsRef)}
+            onMouseEnter={() => addUnderline(projectsNavRef)}
+            onMouseLeave={() => removeUnderline(projectsNavRef)}
+          >
             projects
           </li>
-          <li className="nav-item">
+          <li
+            className="nav-item"
+            ref={resumeNavRef}
+            onMouseEnter={() => addUnderline(resumeNavRef)}
+            onMouseLeave={() => removeUnderline(resumeNavRef)}
+          >
             <a href={resume} target="_blank" id="resume-nav">
               resume
             </a>
           </li>
-          <li className="nav-item" onClick={() => scrollTo(contactRef)}>
+          <li
+            className="nav-item"
+            ref={contactNavRef}
+            onClick={() => scrollTo(contactRef)}
+            onMouseEnter={() => addUnderline(contactNavRef)}
+            onMouseLeave={() => removeUnderline(contactNavRef)}
+          >
             contact
           </li>
         </ul>
